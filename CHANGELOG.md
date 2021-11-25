6d138acd53c998692cd66f1c1d6fdc096ea28543
ronbb <ronbiaobiao@gmail.com>
2021-11-25 10:15:29 +0800

    fix: remove debug print

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
-## 1.0.0
+---
+title: CHANGELOG
+---
+
+## 0.0.1-pre+2
+
+- Remove debug print.
+
+## 0.0.1-pre+1
 
 - Initial version.
--- bin/workbook.dart
+++ bin/workbook.dart
@@ -1,9 +1,5 @@
-import 'dart:io';
-
 import 'package:workbook/src/start.dart';
 
 void main(List<String> args) {
-  print('main');
-  print(Platform.script);
   start();
 }
